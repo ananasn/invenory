56045d07167ea149c8c58593308dd21b0cf1f422
ananasn <ananasn94@gmail.com>
2021-03-11 17:04:33 +0300

    Add base64 image

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from kivy.app import App
 
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy_garden.xcamera import XCamera
 
 import ui
+
+from io import BytesIO
+import base64
+
+
+class MyXCamera(XCamera):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def shoot(self):
+        img = self.export_as_image()
+        buff = BytesIO()
+        buff.seek(0)
+        img.save(buff, fmt='png')
+        with open('test.text', 'w') as f:
+            f.write(base64.b64encode(buff.getvalue()).decode())
+        print("Hello!")
 
 
 class MyApp(App):
